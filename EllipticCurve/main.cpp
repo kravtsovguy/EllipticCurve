048f5ff19645f105dbe4c6ab175cb351358a8ad9
Matvey Kravtsov <kravtsovguy@gmail.com>
2016-06-12 11:09:51 +0300

    Initial Commit

---
+++ EllipticCurve/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EllipticCurve
+//
+//  Created by Matvey Kravtsov on 12.06.16.
+//  Copyright © 2016 Matvey Kravtsov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
